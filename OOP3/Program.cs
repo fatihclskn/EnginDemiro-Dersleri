2158159342e06b56ad7ccb8aa27e4d04d010f3e2
fatih çalışkan <fatihclskn132@gmail.com>
2021-06-09 03:19:48 +0300

    09 HAZİRAN 2021

---
+++ OOP3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IKrediManager
+            IhtiyaçKrediManager ihtiyaçKrediManager = new IhtiyaçKrediManager();
+            
+            //IKrediManager
+            TaşıtKrediManager taşıtKrediManager = new TaşıtKrediManager();
+
+            //IKrediManager
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileloggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(taşıtKrediManager, fileloggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyaçKrediManager , konutKrediManager , taşıtKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+            Console.ReadLine();
+        }
+    }
+}
